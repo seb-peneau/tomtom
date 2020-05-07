e738a3ad40e14f2a28cd16509f79c589c8c63e09
sebastien <sebastien.peneau@orange.com>
2020-05-07 13:55:52 +0200

    Create tomtom program for a Mario animation.

---
+++ src/components/display/tomtom/tomtom.cpp
@@ -0,0 +1,76 @@
+#include "tomtom.h"
+
+
+TomtomDisplayImpl::TomtomDisplayImpl() {
+  display = new PxMATRIX(32,16,P_LAT, P_OE,P_A,P_B,P_C);
+}
+
+void TomtomDisplayImpl::setup () {
+  display->begin(8);
+  display->setFastUpdate(true);
+  clearDisplay();
+  display->setCursor(2,0);
+  display->setFont(&Picopixel);
+  display->setTextColor(display->color565(125,125,125));
+  display->print("192.168.15.3");
+  display_ticker = new Ticker();
+  display_ticker->attach_ms(2, &TomtomDisplayImpl::display_updater, this);
+}
+
+void TomtomDisplayImpl::loop() {
+  drawFrame(mario_frame_1);
+  drawFrame(mario_frame_2);
+  drawFrame(mario_frame_3);
+  drawFrame(mario_frame_4);
+  drawFrame(mario_frame_5);
+  drawFrame(mario_frame_6);
+  drawFrame(mario_frame_7);
+  drawFrame(mario_frame_8);
+  drawFrame(mario_frame_9);
+  drawFrame(mario_frame_10);
+  drawFrame(mario_frame_11);
+  drawFrame(mario_frame_12);
+}
+
+// ISR for display refresh
+void TomtomDisplayImpl::display_updater(TomtomDisplayImpl *tomtomDisplay)
+{
+  tomtomDisplay->display->display(tomtomDisplay->display_draw_time);
+}
+
+void TomtomDisplayImpl::clearDisplay () {
+  display->clearDisplay();
+}
+
+void TomtomDisplayImpl::drawFrame(uint16_t *frame) {
+  clearDisplay();
+  int imageHeight = 16;
+  int imageWidth = 32;
+  int counter = 0;
+  for (int yy = 0; yy < imageHeight; yy++) {
+    for (int xx = 0; xx < imageWidth; xx++) {
+      display->drawPixel(xx, yy, frame[counter]);
+      counter++;
+    }
+  }
+  delay(100);
+}
+
+
+void TomtomDisplayImpl::displayHour(int hour, int minutes) {
+}
+
+void TomtomDisplayImpl::setCoordStatus (int i, int j, bool state) {
+}
+
+void TomtomDisplayImpl::displayWifiStatus (bool wifiStatus) {
+}
+
+void TomtomDisplayImpl::displayTemp (int temp) {
+}
+
+void TomtomDisplayImpl::displayConfigurationMode (bool confEnabled) {
+}
+
+void TomtomDisplayImpl::displayOtaProgress (int progressPercent) {
+}
---
+++ src/components/display/tomtom/tomtom.h
@@ -0,0 +1,44 @@
+#include "../displayInterface.h"
+
+// I don't get why I need this include here
+#include <Wire.h>
+// TODO : should remove it
+
+#include <PxMatrix.h>
+#include <Ticker.h>
+#include <Adafruit_GFX.h>
+#include <Fonts/TomThumb.h>
+#include <Fonts/Picopixel.h>
+#include <Fonts/Tiny3x3a2pt7b.h>
+#include <Fonts/Org_01.h>
+
+#include "animations/mario.h"
+
+#define P_LAT 16
+#define P_A 12
+#define P_B 0
+#define P_C 15
+#define P_OE 2
+
+class TomtomDisplayImpl : public DisplayInterface
+{
+  private:
+    PxMATRIX *display;
+    Ticker *display_ticker;
+    uint8_t display_draw_time=20; //10-50 is usually fine
+    static void display_updater (TomtomDisplayImpl *ledDisplay);
+    void drawFrame (uint16_t *frame);
+
+  public:
+    TomtomDisplayImpl();
+    void setup ();
+    void loop ();
+    void clearDisplay ();
+    void setCoordStatus (int x, int y, bool state);
+    void displayWifiStatus (bool wifiStatus);
+    void displayHour(int hour, int minute);
+    void displayTemp(int temp);
+    void displayConfigurationMode(bool confEnabled);
+    void displayOtaProgress (int progressPercent);
+
+};
